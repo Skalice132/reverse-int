ff456b256cfe0d55cb2a783aa34db39d40c6664f
Skalice132 <58006458+Skalice132@users.noreply.github.com>
2020-10-21 22:36:13 +0400

    init: complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+	n = Math.abs(n);
+  let str = String(n);
+  let result = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+  	result += str[i]
+  }
+  return result;
 }
